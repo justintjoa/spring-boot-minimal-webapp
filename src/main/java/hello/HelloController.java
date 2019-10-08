425d44050e69e13a76c46c0df12646e9fcd01214
Justin Tjoa <justintjoa@umail.ucsb.edu>
2019-10-08 11:03:13 -0700

    second

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-	public String index() {
+	public static String index() {	
 		String html = "<h1>Hello World!</h1>\n" + "<p>This web app is powered by \n" + "<a href='https://github.com/justintjoa/spring-boot-minimal-webapp'>this github repo</a></p>\n";
 		return html;
-	}   
-
+	}
+	public static void main(String[] args) {
+    		HelloController.index();
+	}	
  
 }
