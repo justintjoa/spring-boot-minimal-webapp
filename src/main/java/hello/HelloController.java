cc53896603a7457820b4cecbf759a9650ef06eb5
Justin Tjoa <justintjoa@umail.ucsb.edu>
2019-10-08 11:36:42 -0700

    fourth

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-	@RequestMapping("/")
-    	public String index() {
-    		String html = "<h1>Hello World!</h1>\n" +
-   	 	"<p>This web app is powered by \n" +
-    		"<a href='https://github.com/mygithubid/spring-boot-minimal-webapp'>this github repo</a></p>\n";
-		return html;
-	} 
+    @RequestMapping("/")
+    public String index() {
+    	String html = "<h1>Hello World!</h1>\n" +
+   	 "<p>This web app is powered by \n" +
+    	"<a href='https://github.com/mygithubid/spring-boot-minimal-webapp'>this github repo</a></p>\n";
+	return html;
+     }
 }
